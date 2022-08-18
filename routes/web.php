9074f81b653f05078a981206af9f4412ad17918b
Nerdy <gedoniani@gmail.com>
2022-08-18 09:13:10 +0100

    Auth Scaffolding

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('/gen', function () {
     dd(Uuid::generate()->string);
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
--- vite-project/src/views/Users.vue
+++ vite-project/src/views/Users.vue
@@ -4,6 +4,7 @@
             <div class="flex items-center justify-between">
                 <h1 class="text-3xl font-bold text-gray-900">Users</h1>
 
+
             </div>
         </template>
         <div v-if="users.loading" role="status" class="flex justify-center">
@@ -36,7 +37,7 @@
                             class="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold">
                             Status
                         </th>
-                
+
                         <th
                             class="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold">
                         </th>
@@ -78,22 +79,16 @@
 
                         <td
                             class="flex font-bold border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                         <a 
-                        
-                          target="_blank"
-                          class="h-8 w-8 flex items-center justify-center rounded-full
+                            <a target="_blank" class="h-8 w-8 flex items-center justify-center rounded-full
                           border border-transparent text-sm text-indigo-500 focus:ring-2
-                          focus:ring-offset-2 focus:ring-indigo-500"
-                          >
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
-                            </svg>
-                        </a>
-                         <button
-                            v-if="auser.id"
-                            type="button"
-                            @click="emit('delete', auser)"
-                            class="
+                          focus:ring-offset-2 focus:ring-indigo-500">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                                    stroke="currentColor" stroke-width="2">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+                                </svg>
+                            </a>
+                            <button v-if="auser.id" type="button" @click="emit('delete', auser)" class="
                             h-8
                             w-8
                             flex
@@ -104,13 +99,15 @@
                             rounded-full
                             focus:ring-2 focus:ring-offset-2 focus:ring-red-500
                        ">
-                            <!-- Svg trash goes here -->
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                            </svg>
-                        </button>
+                                <!-- Svg trash goes here -->
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                                    stroke="currentColor" stroke-width="2">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                </svg>
+                            </button>
                         </td>
-                   
+
                     </tr>
 
                 </tbody>
